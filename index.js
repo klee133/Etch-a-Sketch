0a348d4c24484e36458b0e7f07edf93a0dd2f23e
klee133 <61660035+klee133@users.noreply.github.com>
2025-10-28 15:50:15 -0400

    Rewrite code into a function

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for(let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+function createGrid(num) {
+    for(let i = 0; i < num; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for(let j = 0; j < num; j++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+
+createGrid(16);
